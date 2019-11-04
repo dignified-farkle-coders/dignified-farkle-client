8b6cae2e00dd85e721c5f9042de074df7379f84d
Rlong1218 <7dbrobert1218@gmail.com>
2019-11-04 15:36:29 -0700

    Market buttons.

--- app/src/main/java/io/farkle/dignified_farkle_client/MarketFrag.java
+++ app/src/main/java/io/farkle/dignified_farkle_client/MarketFrag.java
@@ -4,19 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class MarketFrag extends Fragment {
+public class MarketFrag extends Fragment implements View.OnClickListener {
   View view;
+  private int points = 69000;
+  private TextView pointsTextView;
+  private Button button;
+  private TextView textView;
 
   @Nullable
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     view = inflater.inflate(R.layout.fragment_market, container, false);
+    textView = (TextView) view.findViewById(R.id.points);
+    textView.setText(getString(R.string.points, points));
+    button = view.findViewById(R.id.market_item_one);
+    button.setOnClickListener(this);
+
     return view;
+  }
+  @Override
+  public void onClick(View view) {
+    points -= 200;
+    textView.setText(getString(R.string.points, points));
+
   }
 }
 
