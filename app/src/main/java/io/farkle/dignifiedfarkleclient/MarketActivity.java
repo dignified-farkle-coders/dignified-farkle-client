7f819576f84c14914f8e4d14673db58a2089d312
James Larivee <mr_15th_entertainer@yahoo.com>
2019-12-12 18:14:57 -0700

    Player dice color SharedPreferences added

--- app/src/main/java/io/farkle/dignifiedfarkleclient/MarketActivity.java
+++ app/src/main/java/io/farkle/dignifiedfarkleclient/MarketActivity.java
@@ -1,12 +1,16 @@
 package io.farkle.dignifiedfarkleclient;
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import io.farkle.dignifiedfarkleclient.model.entity.Player;
 
@@ -20,14 +24,22 @@ public class MarketActivity extends AppCompatActivity {
   private Button buyButton;
   ImageView upArrow;
   ImageView downArrow;
+  ImageView userDie;
   private int basket = 0;
   private int dieCount = 1;
+  private TextView dieId;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     setContentView(R.layout.activity_market);
     super.onCreate(savedInstanceState);
+    SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0); // 0 - for private mode
+    Editor editor = pref.edit();
 
+    dieId = findViewById(R.id.die_id);
+    dieId.setText(Html.fromHtml("&#35; " + 1));
+    userDie = findViewById(R.id.die_1);
+    userDie.setColorFilter(Color.parseColor(pref.getString(String.valueOf(1), "#00FFFFFF"))); // getting String);
     upArrow = findViewById(R.id.scroll_up);
     downArrow = findViewById(R.id.scroll_down);
     downArrow.setVisibility(View.INVISIBLE);
@@ -47,6 +59,8 @@ public class MarketActivity extends AppCompatActivity {
       @Override
       public void onClick(View v) {
         basket = 200;
+        userDie.setColorFilter(Color.argb(100, 255, 0, 0));
+        editor.putString(String.valueOf(dieCount), "#64FF0000"); // Storing string
       }
     });
 
@@ -54,6 +68,9 @@ public class MarketActivity extends AppCompatActivity {
       @Override
       public void onClick(View v) {
         basket = 200;
+        userDie.setColorFilter(Color.argb(100, 0, 255, 0));
+        editor.putString(String.valueOf(dieCount), "#6400FF00"); // Storing string
+
       }
     });
 
@@ -61,6 +78,9 @@ public class MarketActivity extends AppCompatActivity {
       @Override
       public void onClick(View v) {
         basket = 200;
+        userDie.setColorFilter(Color.argb(100, 0, 0, 255));
+        editor.putString(String.valueOf(dieCount), "#640000FF"); // Storing string
+
       }
     });
 
@@ -68,6 +88,9 @@ public class MarketActivity extends AppCompatActivity {
       @Override
       public void onClick(View v) {
         basket = 200;
+        userDie.setColorFilter(Color.argb(0, 255, 255, 255));
+        editor.putString(String.valueOf(dieCount), "#00000000"); // Storing string
+
       }
     });
 
@@ -79,18 +102,24 @@ public class MarketActivity extends AppCompatActivity {
           System.out.println(StartFragment.playerPoints - basket);
           StartFragment.player.setVictoryPoints(StartFragment.playerPoints - basket);
           victoryPoints.setText(String.valueOf(StartFragment.player.getVictoryPoints()));
-        }
+          editor.commit(); // commit changes
+        } else {
+          Toast.makeText(getApplicationContext(),"Not Enough Funds",Toast.LENGTH_SHORT).show();          }
       }
     });
 
       upArrow.setOnClickListener(new OnClickListener() {
         @Override
         public void onClick(View v) {
+          if(dieCount != 6) {
+            dieCount += 1;
+          }
+          userDie.setColorFilter(Color.parseColor(pref.getString(String.valueOf(dieCount), "#00FFFFFF"))); // getting String);
+          dieId.setText(Html.fromHtml("&#35; " + dieCount));
           if(dieCount < 6) {
             System.out.println(dieCount);
             downArrow.setVisibility(View.VISIBLE);
             upArrow.setVisibility(View.VISIBLE);
-            dieCount += 1;
           } else {
             upArrow.setVisibility(View.INVISIBLE);
           }
@@ -100,13 +129,17 @@ public class MarketActivity extends AppCompatActivity {
       downArrow.setOnClickListener(new OnClickListener() {
         @Override
         public void onClick(View v) {
+          if(dieCount != 1) {
+            dieCount -= 1;
+          }
+          userDie.setColorFilter(Color.parseColor(pref.getString(String.valueOf(dieCount), "#00FFFFFF"))); // getting String);
+          dieId.setText(Html.fromHtml("&#35; " + dieCount));
           if(dieCount > 1) {
             System.out.println(dieCount);
             downArrow.setVisibility(View.VISIBLE);
             upArrow.setVisibility(View.VISIBLE);
-            dieCount -= 1;
           } else {
-            upArrow.setVisibility(View.INVISIBLE);
+            downArrow.setVisibility(View.INVISIBLE);
           }
         }
       });
